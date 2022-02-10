74938f64c5cfc22bbcb544a9ffa9d9c97cbf6db3
Joao Martins <jvictorweb@gmail.com>
2022-02-10 15:24:54 -0300

    Checkmate logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -36,10 +36,10 @@ public class UI {
 
 	// https://stackoverflow.com/questions/2979383/how-to-clear-the-console
 	public static void clearScreen() {
-		//System.out.println("\33[H\033[23");
+		// System.out.println("\33[H\033[23");
 		System.out.print("\033[H\033[2J");
 		System.out.flush();
-		
+
 	}
 
 	public static ChessPosition readChessPosition(Scanner sc) {
@@ -60,12 +60,16 @@ public class UI {
 		System.out.println();
 		System.out.println("Turn : " + chessMatch.getTurn());
 		System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
-		if(chessMatch.getCheck()) {
-			System.out.println("CHECK");
+		if (!chessMatch.getCheckMate()) {
+			if (chessMatch.getCheck()) {
+				System.out.println("CHECK");
+			}
+		}else {
+			System.out.println("CHECKMATE!");
+			System.out.println("Winner: " + chessMatch.getCurrentPlayer());
 		}
-		
 	}
-	
+
 	public static void printBoard(ChessPiece[][] pieces) {
 		for (int i = 0; i < pieces.length; i++) {
 			System.out.print((8 - i) + " ");
@@ -76,7 +80,7 @@ public class UI {
 		}
 		System.out.println("  a b c d e f g h");
 	}
-	
+
 	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
 		for (int i = 0; i < pieces.length; i++) {
 			System.out.print((8 - i) + " ");
@@ -103,24 +107,24 @@ public class UI {
 		}
 		System.out.print(" ");
 	}
-	
-	
-	
+
 	private static void printCapturedPieces(List<ChessPiece> captured) {
-		List<ChessPiece> white = captured.stream().filter(x -> x.getColor() == Color.WHITE).collect(Collectors.toList());
-		List<ChessPiece> black = captured.stream().filter(x -> x.getColor() == Color.BLACK).collect(Collectors.toList());
-		
+		List<ChessPiece> white = captured.stream().filter(x -> x.getColor() == Color.WHITE)
+				.collect(Collectors.toList());
+		List<ChessPiece> black = captured.stream().filter(x -> x.getColor() == Color.BLACK)
+				.collect(Collectors.toList());
+
 		System.out.println("Captured pieces:");
 		System.out.print("White: ");
 		System.out.print(ANSI_WHITE);
 		System.out.println(Arrays.toString(white.toArray()));
 		System.out.print(ANSI_RESET);
-		
+
 		System.out.println("Captured pieces:");
 		System.out.print("Black: ");
 		System.out.print(ANSI_YELLOW);
 		System.out.println(Arrays.toString(black.toArray()));
 		System.out.print(ANSI_RESET);
 	}
-	
+
 }
