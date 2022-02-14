91fc7504bda7569c8d7cc6dd1fd52b7097dac90c
Joao Martins <jvictorweb@gmail.com>
2022-02-14 16:39:36 -0300

    Promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -39,6 +39,11 @@ public class Program {
 				if(capturedpPiece != null) {
 					captured.add(capturedpPiece);
 				}
+				if (chessMatch.getPromoted() != null) {
+					System.out.println("Enter piece for promotion (B/N/R/Q): ");
+					String type = sc.nextLine();
+					chessMatch.replacePromotedPiece(type);
+				}
 				
 				}catch(ChessException e){
 					System.out.println(e.getMessage());
